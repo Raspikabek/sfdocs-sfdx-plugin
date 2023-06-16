cedd3a689264538a779eb39bfa5e5839103611a8
Eduardo Asenjo <asenjo.rodriguez.eduardo@gmail.com>
2023-06-17 00:16:56 +0100

    feat: add test generate test classes

---
+++ test/commands/sfdocs/generate.test.ts
@@ -0,0 +1,45 @@
+import { expect, test } from '@oclif/test';
+import { DocsGenerateResult } from '../../../src/commands/sfdocs/generate';
+
+describe('Generate Docs', () => {
+  test
+    .stdout()
+    .command(['sfdocs:generate'])
+    .it('runs generation of documentation with defaults', (ctx) => {
+      expect(ctx.stdout).to.contain('json');
+      expect(ctx.stdout).to.contain('docs');
+    });
+
+  test
+    .stdout()
+    .command(['sfdocs:generate', '--json'])
+    .it('runs generate --json (runs with defaults)', (ctx) => {
+      const { result } = JSON.parse(ctx.stdout) as { result: DocsGenerateResult };
+      expect(result.format).to.equal('json');
+      expect(result.outputdir).to.equal('docs');
+    });
+
+  test
+    .stdout()
+    .command(['sfdocs:generate', '--format', 'markdown'])
+    .it('runs generate --format markdown', (ctx) => {
+      expect(ctx.stdout).to.contain('markdown');
+      expect(ctx.stdout).to.contain('docs');
+    });
+
+  test
+    .stdout()
+    .command(['sfdocs:generate', '--format', 'markdown', '--json'])
+    .it('runs generate --format markdown --json', (ctx) => {
+      const { result } = JSON.parse(ctx.stdout) as { result: DocsGenerateResult };
+      expect(result.format).to.equal('markdown');
+    });
+
+  test
+    .stdout()
+    .command(['sfdocs:generate', '--outputdir', 'alternative-dir', '--json'])
+    .it('runs generate --format markdown --json', (ctx) => {
+      const { result } = JSON.parse(ctx.stdout) as { result: DocsGenerateResult };
+      expect(result.outputdir).to.equal('alternative-dir');
+    });
+});
