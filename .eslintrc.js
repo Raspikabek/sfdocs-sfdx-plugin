881105fb3f01d8cb3ff325c1afefe59fd7e583f4
Eduardo Asenjo <asenjo.rodriguez.eduardo@gmail.com>
2023-06-17 00:02:02 +0100

    feat: update jsdoc dependency and remove deleted rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   extends: ['eslint-config-salesforce-typescript', 'plugin:sf-plugin/recommended'],
   root: true,
+  rules: {
+    'jsdoc/newline-after-description': 'off',
+  },
 };
