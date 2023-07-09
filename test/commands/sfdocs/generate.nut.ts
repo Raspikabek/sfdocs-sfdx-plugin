8539517b8e9d702b85d405dd0688fcac29faef1b
Eduardo Asenjo <asenjo.rodriguez.eduardo@gmail.com>
2023-07-09 17:28:58 +0100

    feat: add new nut test scenario

--- test/commands/sfdocs/generate.nut.ts
+++ test/commands/sfdocs/generate.nut.ts
@@ -30,6 +30,14 @@ describe('docs generate NUTs', () => {
     expect(result?.format).to.equal('markdown');
   });
 
+  it('should generate docs in markdown and sfdocs folder', () => {
+    const result = execCmd<DocsGenerateResult>('docs generate --output-dir sfdocs --format markdown --json', {
+      ensureExitCode: 0,
+    }).jsonOutput?.result;
+    expect(result?.format).to.equal('markdown');
+    expect(result?.outputdir).to.equal('sfdocs');
+  });
+
   it('should generate docs with reset option and not contain previous files', () => {
     // fs.mkdirSync('docs/force-app/objects', { recursive: true });
     // fs.writeFileSync('docs/force-app/objects/CustomObject.json', '{test: true}');
